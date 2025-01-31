838f76cd0e5d90341ed429142faf13aeb5ab5068
Thomas Kneller <Thomas.kneller@yahoo.com>
2025-01-31 12:12:09 +0000

    created dev tools page for testing unimplemented functionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="devtools"
         options={{
-          title: 'Explore',
+          title: 'Dev',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
