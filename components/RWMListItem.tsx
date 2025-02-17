fca797136428ef3ad34f44d99a4e93449a660b4e
Thomas Kneller <Thomas.kneller@yahoo.com>
2025-02-17 13:11:04 +0000

    created a more finalised layout structure for route list items

--- components/RWMListItem.tsx
+++ components/RWMListItem.tsx
@@ -1,6 +1,6 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 
 import { Route } from '../app/models';
 
@@ -10,20 +10,33 @@ export const RWMListItem: React.FC<{
 }> = ({ routeData: { idroutes, routeName, lat, long, distance, pace, hostName } }) => {
 
     return (
-        <ThemedView>
+        <ThemedView style={{ backgroundColor: '#002531', marginTop: 5 }}>
             <ThemedText type='subtitle' style={{
-                color: 'red'
+                color: 'white'
             }}>{routeName}
             </ThemedText>
-            <Image></Image>
-            <ThemedText type='defaultSemiBold'>Distance away:</ThemedText>
-            {/* <ThemedText id='distanceFromUser'>{item.distanceFromUser} miles</ThemedText> */}
-            <ThemedText type='defaultSemiBold'>Approximate Route Distance:</ThemedText>
-            <ThemedText id='routeDistance'>{distance} miles</ThemedText>
-            <ThemedText type='defaultSemiBold'>Estimated Pace:</ThemedText>
-            <ThemedText id='routePace'>{pace} min/mi</ThemedText>
-            <ThemedText type='defaultSemiBold'>Hosted by:</ThemedText>
-            <ThemedText id='routeHost'>{hostName}</ThemedText>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+                <Image style={{ height: 25, width: 25, margin: 4 }} source={require("@/assets/images/avatar.png")} />
+                <ThemedText style={{ fontSize: 12 }} type='defaultSemiBold'>Host: </ThemedText>
+                <ThemedText id='routeHost'>{hostName}</ThemedText>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                <View style={{ flexDirection: 'column' }}>
+                    <ThemedText style={{ fontSize: 12 }} type='defaultSemiBold'>Distance away:</ThemedText>
+                    {/* <ThemedText id='distanceFromUser'>{item.distanceFromUser} miles</ThemedText> */}
+                    <ThemedText id='distanceFromUser'>100 miles</ThemedText>
+                </View>
+                <View style={{ flexDirection: 'column' }}>
+                    <ThemedText style={{ fontSize: 12 }} type='defaultSemiBold'>Route Distance:</ThemedText>
+                    <ThemedText id='routeDistance'>{distance} miles</ThemedText>
+                </View>
+                <View style={{ flexDirection: 'column' }}>
+                    <ThemedText style={{ fontSize: 12 }} type='defaultSemiBold'>Estimated Pace:</ThemedText>
+                    <ThemedText id='routePace'>{pace} min/mi</ThemedText>
+                </View>
+            </View>
+
+            <Image style={{ height: 150, width: '100%', flex: 1, justifyContent: 'center' }} source={require("@/assets/images/placeholdermap.png")} />
         </ThemedView>
     );
 };
